4c0c3583afa2fe9a54317b86c166039066710c45
pedrobellinetti <pedrobellinetti9@gmail.com>
2026-04-22 11:22:11 -0300

    feat: adiciona banco de conhecimento da função 1 e 2

---
+++ projeto.pl
@@ -0,0 +1,28 @@
+% Base de conhecimento
+
+% "Função curso(cc, nc)"
+% cc = curso com código
+% nc = nome do curso
+
+curso(1, informatica).
+curso(2, eletro_eletronica).
+curso(3, engenharia_de_software).
+curso(4, engenharia_de_computacao).
+curso(5, engenharia_civil).
+curso(6, engenharia_quimica).
+% materia(cm, nm, am)
+% cm = matéria com código
+% nm = nome da matéria
+% am = quantidade de aulas por semana
+materia(1, topicos_de_tecnologia_e_de_programacao).
+materia(2, estudos_avancados_de_banco_de_dados).
+materia(3, introducao_a_inteligencia_artificial_e_machine_learning).
+materia(4, teoria_das_organizacoes).
+materia(5, padroes_e_arquitetura_de_software).
+materia(6, estatistica).
+
+
+
+% curriculo(cc, [cm1, cm2, ..., cmn])
+% cc = curso com código
+% cm = matérias com códigos
